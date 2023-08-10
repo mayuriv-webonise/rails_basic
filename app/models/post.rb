c5a18ccddb76950acf7a66678b68a9285a454f94
mayuriv-webonise <mayuri.vibhute@webonise.com>
2023-08-10 11:24:20 +0530

    unnecessary code removed and spaced removed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,22 +1,4 @@
 class Post < ApplicationRecord
     validates :title, presence: true
     validates :content, presence: true
-    scope :published, -> { where(published: true) }
-    scope :unpublished, -> { where(published: false) }
-
-    def published?
-      published
-    end
-
-    def unpublished?
-      !published
-    end
-
-    def publish
-      update(published: true)
-    end
-
-    def unpublish
-      update(published: false)
-    end
-  end
+end
